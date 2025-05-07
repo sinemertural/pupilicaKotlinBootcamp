8e4d76ea516a5930e0a65298ec7dd3b98f51393d
Sinem Ertural <sinemertural1655@gmail.com>
2025-05-07 18:59:24 +0300

    dosyalar yuklendı

--- WidgetsKullanimi/app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
+++ WidgetsKullanimi/app/src/main/java/com/example/widgetskullanimi/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.widgetskullanimi.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,13 +22,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.toggleButton.addOnButtonCheckedListener { group, checkedId, isChecked ->
             kontrol = isChecked
-            if (isChecked){
+            if(kontrol){
                 val secilenButton = findViewById<Button>(checkedId)
                 val buttonYazi = secilenButton.text.toString()
-                Log.e("sonuc", buttonYazi)
-
+                Log.e("Sonuç",buttonYazi)
             }
-
         }
 
         val ulkeler = ArrayList<String>()
@@ -37,18 +37,27 @@ class MainActivity : AppCompatActivity() {
         val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,ulkeler)
         binding.autoCompleteTextView.setAdapter(arrayAdapter)
 
-
-
-
         binding.buttonGoster.setOnClickListener {
-            if (kontrol){
+            if(kontrol){
                 val secilenButton = findViewById<Button>(binding.toggleButton.checkedButtonId)
                 val buttonYazi = secilenButton.text.toString()
-                Log.e("sonuc (göster) :", buttonYazi)
-
+                Log.e("Sonuç ( Göster )",buttonYazi)
             }
             val secilenUlke = binding.autoCompleteTextView.text.toString()
-            Log.e("sonuc :", secilenUlke)
+            Log.e("Sonuç",secilenUlke)
+        }
+
+        binding.buttonAlert.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Başlık")
+                .setMessage("Mesaj")
+                .setPositiveButton("Tamam") {d,i ->
+                    Toast.makeText(this,"Tamam Tıklandı", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton("İptal"){d,i ->
+                    Toast.makeText(this,"İptal Tıklandı", Toast.LENGTH_SHORT).show()
+                }
+                .show()
         }
     }
 }
